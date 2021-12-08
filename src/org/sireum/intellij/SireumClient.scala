b160cba70cced26cf17d2e9d2069b3369b98e087
Robby <robby@santoslab.org>
2021-12-08 11:07:11 -0600

    Generalized cvc4 to cvc to support both cvc4 and cvc5.

--- src/org/sireum/intellij/SireumClient.scala
+++ src/org/sireum/intellij/SireumClient.scala
@@ -393,7 +393,7 @@ object SireumClient {
             timeoutInMs = LogikaConfigurable.timeout,
             useReal = LogikaConfigurable.useReal,
             fpRoundingMode = LogikaConfigurable.fpRoundingMode,
-            cvc4RLimit = LogikaConfigurable.cvcRLimit
+            cvcRLimit = LogikaConfigurable.cvcRLimit
           )
         ),
         if (!(org.sireum.Os.path(project.getBasePath) / "bin" / "project.cmd").exists || p.ext.value == "sc" || p.ext.value == "cmd") {
