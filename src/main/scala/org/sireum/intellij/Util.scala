dd727dbd1eac9463a51b031d7c593ae4a71e35ce
Robby <robby@santoslab.org>
2023-12-07 14:16:42 -0600

    Added Logika support for Linux arm64.

--- src/main/scala/org/sireum/intellij/Util.scala
+++ src/main/scala/org/sireum/intellij/Util.scala
@@ -176,7 +176,6 @@ object Util {
   }
 
   lazy val isLogikaSupportedPlatform: Boolean = org.sireum.Os.kind match {
-    case org.sireum.Os.Kind.LinuxArm => false
     case org.sireum.Os.Kind.Unsupported => false
     case _ => true
   }
